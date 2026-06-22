15bc998ad8e1f8728dc844641e2e78e4db685f57
Omar <omarrivas042@gmail.com>
2026-06-22 17:16:58 -0400

    Se elimina componente innecesario llamado home.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,24 +2,20 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'inicio',
     pathMatch: 'full',
   },
   {
     path: 'inicio',
-    loadComponent: () => import('./inicio/inicio.page').then( m => m.InicioPage)
+    loadComponent: () => import('./inicio/inicio.page').then(m => m.InicioPage)
   },
   {
     path: 'informacion-personal',
-    loadComponent: () => import('./informacion-personal/informacion-personal.page').then( m => m.InformacionPersonalPage)
+    loadComponent: () => import('./informacion-personal/informacion-personal.page').then(m => m.InformacionPersonalPage)
   },
   {
     path: 'contacto',
-    loadComponent: () => import('./contacto/contacto.page').then( m => m.ContactoPage)
+    loadComponent: () => import('./contacto/contacto.page').then(m => m.ContactoPage)
   },
 ];
